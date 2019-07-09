fd299c6911056555b238e973a3e7f581a90dff59
unknown <berkelmas96@gmail.com>
2019-07-10 02:25:52 +0300

    angular universal deployment to firebase hosting with firebase functions

---
+++ functions/index.js
@@ -0,0 +1,41 @@
+const functions = require('firebase-functions');
+//const app = require('./dist/server');
+
+require('zone.js/dist/zone-node');
+
+const express = require('express');
+const {join} = require('path');
+
+// Express server
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+const DIST_FOLDER = join(process.cwd(), 'dist/browser');
+
+// * NOTE :: leave this as require() since this file is built Dynamically from webpack
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+
+// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+app.engine('html', ngExpressEngine({
+  bootstrap: AppServerModuleNgFactory,
+  providers: [
+    provideModuleMap(LAZY_MODULE_MAP)
+  ]
+}));
+
+app.set('view engine', 'html');
+app.set('views', DIST_FOLDER);
+
+// Example Express Rest API endpoints
+// app.get('/api/**', (req, res) => { });
+// Serve static files from /browser
+app.get('*.*', express.static(DIST_FOLDER, {
+  maxAge: '1y'
+}));
+
+// All regular routes use the Universal engine
+app.get('*', (req, res) => {
+  res.render('index-1', { req });
+});
+
+exports.ssr = functions.https.onRequest(app);
